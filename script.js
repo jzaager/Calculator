31bbd24f27c02cd78769b0c4640f81808f7729eb
Justin Zaager <hextechprotect@gmail.com>
2022-07-30 15:22:24 -0400

    Add functionality when pressing equals button
    Store values before and after operator is found. When equals button
    is pressed, call operator function and update display with result.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// VARIABLES //
+let displayValue;
+const displayArea = document.querySelector('.calc-screen');
+const buttons = document.querySelectorAll('.buttons');
+const equalsBtn = document.getElementById('.equals');
 
 // CALCULATION FUNCTIONS
 const add = (a, b) => a + b;
@@ -9,22 +14,50 @@ const divide = (a, b) => a / b;
 const operate = function(operator, firstNum, secondNum) {
   if (operator === '+') return add(firstNum, secondNum);
   if (operator === '-') return subtract(firstNum, secondNum);
-  if (operator === '*') return multiply(firstNum, secondNum);
-  if (operator === '/') return divide(firstNum, secondNum);
+  if (operator === 'x') return multiply(firstNum, secondNum);
+  if (operator === '÷') return divide(firstNum, secondNum);
 }
 
-// POPULATE DISPLAY UPON NUMBER PRESS
-const displayArea = document.querySelector('.calc-screen');
-const numberBtns = document.querySelectorAll('.number-buttons');
-let displayValue;
+// EVENT LISTENERS //
+buttons.forEach(button => button.addEventListener('click', (e) => 
+  updateDisplay(e)));
 
-numberBtns.forEach(button => button.addEventListener('click', (e) => {
+// FUNCTIONS //
+
+// UPDATE DISPLAY FUNCTION
+function updateDisplay(e) {
   if (e.target.id === 'clear') {
     displayArea.textContent = null;
-    displayValue = null;
-    return displayValue;
+    return displayValue = null;
   }
+   if (e.target.id === 'equals') {
+    displayValue = operate(getOperator(displayValue), +getFirstValue(displayValue), +getSecondValue(displayValue));
+    return displayArea.textContent = displayValue;
+  }
+
   displayArea.textContent += e.target.id;
   displayValue = displayArea.textContent;
   console.log(displayValue);
-}));
+}
+
+// GET PARAMETERS FOR OPERATE FUNCTION
+function getOperator(string) {
+  for (let i = 0; i < string.length; i++) {
+    if (isNaN(+string[i])) 
+      return string[i]
+  }
+}
+
+function getFirstValue(string) {
+  for (let i = 0; i < string.length; i++) {
+    if (isNaN(+string[i])) 
+      return string.substring(0, i);
+  }
+}
+
+function getSecondValue(string) {
+  for (let i = 0; i < string.length; i++) {
+    if (isNaN(+string[i])) 
+      return string.substring((i + 1));
+  }
+}
